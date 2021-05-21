96585296eb75d8028cf14bcdb50e8fca3ca433cc
msturn2 <82980444+msturn2@users.noreply.github.com>
2021-05-21 16:05:08 -0700

    Final w/ Search Bar
    Added comments in order to clearly explain my coding decisions.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-//Global variables
+//global variables
 const itemsPerPage = 9;
 const studentList = document.querySelector(".student-list");
 const linkList = document.querySelector(".link-list");
@@ -19,12 +19,15 @@ const header = document.querySelector(".header");
 
 
 function showPage(list, page) {
+   //variables to set min and max range per page
    const startIndex = (page * itemsPerPage) - itemsPerPage;
    const endIndex = page * itemsPerPage;
    studentList.innerHTML = "";
- 
+   
    for (let i = 0; i < list.length; i++) {
+      //conditional to display students by page
       if (i >= startIndex && i < endIndex) {
+         //vaiable using object literal to push to HTML
          const studentItem = 
          `<li class="student-item cf">
             <div class="student-details">
@@ -43,10 +46,12 @@ function showPage(list, page) {
 }
 
 function addPagination(list) {
+   //varialbe to handle total number of pages to display
    const numOfPages = Math.ceil(list.length / itemsPerPage);
    linkList.innerHTML = "";
    
    for (let i = 1; i <= numOfPages; i++) {
+      //object literal to number the pagination buttons
       const button = 
       `<li>
          <button type="button">${i}</button>
@@ -56,10 +61,10 @@ function addPagination(list) {
       const buttonClassName = document.querySelector(".link-list button");
       buttonClassName.className = "active";
    }
-   
+   //event listener to move between paginated pages
    linkList.addEventListener ("click", (e) => {
       const changeButton = e.target;
-
+      //moving the active class to active button while removing active class from previous button
       if (changeButton.tagName === "BUTTON") {
          const prevActiveButton = document.querySelector("button.active");
          prevActiveButton.classList.remove("active");
@@ -68,7 +73,7 @@ function addPagination(list) {
       }
    });
 }
-
+//created html tags for searh bar
 const searchBar = 
    `<label for="search" class="student-search">
       <span>Search by name</span>
@@ -78,18 +83,21 @@ const searchBar =
 
 header.insertAdjacentHTML("beforeend", searchBar);
 
+//function to capture user input and compare against array in data 
 function showSearchMatches(usersSearch, list) {
    const searchList = [];
    for (let i = 0; i < list.length; i++) {
       const fullName = `${list[i].name.first} ${list[i].name.last}`.toLowerCase();
-
+      //compares user input to object literal from above
       if (fullName.includes(usersSearch.toLowerCase())) {
+         //must use push method in order to populate page with matching data
          searchList.push(list[i]);
       }
    }
    return searchList;
 }
 
+//this fucntion takes in to account empty user input and no returned results
 function searchFunc(usersSearch) {
    const matchSearch = showSearchMatches(usersSearch, data);
    studentList.innerHTML = "";
@@ -104,10 +112,12 @@ function searchFunc(usersSearch) {
    }
 }
 
+//Used "change" rather than "keyup" since there is a button. Left (event) empty.
 search.addEventListener("change", () => {
    search = document.querySelector("#search");
    searchFunc(search.value);
 });
 
+//called functions
 showPage(data, 1);
 addPagination(data);
