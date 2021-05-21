250b9aea4912698ed2238d069c5b985896c41c63
msturn2 <82980444+msturn2@users.noreply.github.com>
2021-05-21 15:39:23 -0700

    Final refactoring w/ SearchBar
    I got the search bar to work finally.  It was a little bit funky at first because I overbuilt the code a bit.  Once I removed the unnecessary lines of code, it worked perfectly.

--- js/script.js
+++ js/script.js
@@ -11,20 +11,103 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+//Global variables
+const itemsPerPage = 9;
+const studentList = document.querySelector(".student-list");
+const linkList = document.querySelector(".link-list");
+const header = document.querySelector(".header");
 
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+function showPage(list, page) {
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+   studentList.innerHTML = "";
+ 
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         const studentItem = 
+         `<li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>`;
+         
+         studentList.insertAdjacentHTML("beforeend", studentItem);
+      }
+   }
+}
 
+function addPagination(list) {
+   const numOfPages = Math.ceil(list.length / itemsPerPage);
+   linkList.innerHTML = "";
+   
+   for (let i = 1; i <= numOfPages; i++) {
+      const button = 
+      `<li>
+         <button type="button">${i}</button>
+      </li>`;
+      
+      linkList.insertAdjacentHTML("beforeend", button);
+      const buttonClassName = document.querySelector(".link-list button");
+      buttonClassName.className = "active";
+   }
+   
+   linkList.addEventListener ("click", (e) => {
+      const changeButton = e.target;
 
+      if (changeButton.tagName === "BUTTON") {
+         const prevActiveButton = document.querySelector("button.active");
+         prevActiveButton.classList.remove("active");
+         changeButton.className = "active";
+         showPage(list, changeButton.textContent);
+      }
+   });
+}
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+const searchBar = 
+   `<label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>`;
 
+header.insertAdjacentHTML("beforeend", searchBar);
 
+function showSearchMatches(usersSearch, list) {
+   const searchList = [];
+   for (let i = 0; i < list.length; i++) {
+      const fullName = `${list[i].name.first} ${list[i].name.last}`.toLowerCase();
 
-// Call functions
+      if (fullName.includes(usersSearch.toLowerCase())) {
+         searchList.push(list[i]);
+      }
+   }
+   return searchList;
+}
+
+function searchFunc(usersSearch) {
+   const matchSearch = showSearchMatches(usersSearch, data);
+   studentList.innerHTML = "";
+   linkList.innerHTML = "";
+
+   if (matchSearch.length === 0) {
+      studentList.innerHTML = `<div><h2 style="font-size: 25px">Unfortunately, no results were found.</h2></div>`;
+   }
+   else {
+      showPage(matchSearch, 1);
+      addPagination(matchSearch);
+   }
+}
+
+search.addEventListener("change", () => {
+   search = document.querySelector("#search");
+   searchFunc(search.value);
+});
+
+showPage(data, 1);
+addPagination(data);
